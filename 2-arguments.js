59c0c99c26a405e6afe248fd7db6f25c3da50339
Aola-j <okunolaabe@gmail.com>
2025-07-12 17:42:48 -0700

    argument-commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
 let argument = 1;
 
-if(argument < 1){
- console.log("No argument");
-}else if(argument == 1){
- console.log("Argument found");
-}else{
- console.log("Arguments found");
+if (argument === 0) {
+  console.log("No argument");
+} else if (argument === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
