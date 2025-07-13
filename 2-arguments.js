18285c7a654bd1b4ab799da757448b9a9d3c1c4a
Aola-j <okunolaabe@gmail.com>
2025-07-12 18:44:30 -0700

    newarg-commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,30 +1,8 @@
-let argument1 = 0;
+const args = process.argv.slice(2);
 
-if (argument1 < 1) {
+if (args.length === 0) {
   console.log("No argument");
-} else if (argument1 === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
-
-
-let argument2 = 1;
-
-if (argument2 < 1) {
-  console.log("No argument");
-} else if (argument2 === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
-
-
-let argument3 = 3;
-
-if (argument3 < 1) {
-  console.log("No argument");
-} else if (argument3 === 1) {
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
