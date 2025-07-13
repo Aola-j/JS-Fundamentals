949f435941477c82e22aa289378041a9066198bf
Aola-j <okunolaabe@gmail.com>
2025-07-12 18:18:19 -0700

    Added arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,30 @@
-let argument = 0;
+let argument1 = 0;
 
-if (argument < 1){
+if (argument1 < 1) {
   console.log("No argument");
-} else if (argument == 1) {
+} else if (argument1 === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
+
+let argument2 = 1;
+
+if (argument2 < 1) {
+  console.log("No argument");
+} else if (argument2 === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
+
+let argument3 = 3;
+
+if (argument3 < 1) {
+  console.log("No argument");
+} else if (argument3 === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
